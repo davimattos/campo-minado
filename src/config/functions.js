fbf10d54e34aaaeab568fa37e989169351124f7f
davimattos <davimattosdev@gmail.com>
2019-12-24 16:23:57 -0300

    verificando pendências nos campos

--- src/config/functions.js
+++ src/config/functions.js
@@ -92,4 +92,7 @@ const fields = board => [].concat(...board);
 const hadExplosion = board =>
   fields(board).filter(field => field.exploded).length > 0;
 
+const pendding = field =>
+  (field.mined && !field.flagged) || (!field.mined && !field.opened);
+
 export {createMinedBoard};
